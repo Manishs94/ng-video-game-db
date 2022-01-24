a3109277b1882a6e4d5c3b0f456d343c81f29f93
Manish Sondhi <manishsondhi01@gmail.com>
2022-01-23 19:14:08 -0500

    Update 1

---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  BASE_URL: 'https://rawg-video-games-database.p.rapidapi.com',
+  firebase: {
+    apiKey: "AIzaSyAiML8BPSemqCCKxgx0dkZCh00UP8vJQMo",
+    authDomain: "ng-video-game-db.firebaseapp.com",
+    databaseURL: "https://ng-video-game-db-default-rtdb.firebaseio.com",
+    projectId: "ng-video-game-db",
+    storageBucket: "ng-video-game-db.appspot.com",
+    messagingSenderId: "952418778174",
+    appId: "1:952418778174:web:43213d621ab67b0975256f",
+    measurementId: "G-CG7K7QZ45N"
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
